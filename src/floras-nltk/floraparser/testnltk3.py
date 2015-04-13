8a1f738484b42eeabb8e21d308faa805e5714358
George Gosline <g.gosline@kew.org>
2015-04-13 17:42:32 +0100

    Merge branch 'master' of https://github.com/ggosline/taxonparser
    Conflicts:
    	src/floras-nltk/floras_nltk.py

--- src/floras-nltk/floraparser/testnltk3.py
+++ src/floras-nltk/floraparser/testnltk3.py
@@ -1,5 +1,7 @@
 __author__ = 'Geoge'
 
+import sys
+
 import nltk
 from nltk import grammar, parse
 from nltk.grammar import FeatureGrammar, FeatStructNonterminal, FeatStructReader, read_grammar, SLASH, TYPE, Production
@@ -26,7 +28,7 @@ class FGFlora(FeatureGrammar):
         (only if features and logic_parser is None)
         """
         if features is None:
-            features = (SLASH, TYPE)
+            features = (TYPE, SLASH)
 
         if fstruct_reader is None:
             fstruct_reader = FeatStructReader(features, FeatStructNonterminal,
@@ -49,7 +51,7 @@ class FGFlora(FeatureGrammar):
 
 trec = defaultdict(lambda: None)
 
-description = 'lamina dull, (3·3)4·4–10·8(15) × (1·2)2·1–4·5 cm., reddish, oblong to elliptic-oblong'  # , acuminate, obtuse or retuse, with margin shallowly rounded-denticulate or rarely subentire, cuneate to rounded, chartaceous to softly coriaceous' #, with (6)7–10 lateral nerves and ± densely reticulate venation varying in prominence'
+description = 'lamina dull on both sides, (3·3)4·4–10·8(15) × (1·2)2·1–4·5 cm., reddish, oblong to elliptic-oblong,  acuminate, obtuse or retuse, with margin shallowly rounded-denticulate or rarely subentire, cuneate to rounded, chartaceous to softly coriaceous, with (6)7–10 lateral nerves'  # and ± densely reticulate venation varying in prominence'
 
 trec['description'] = description
 trdr = [trec]
@@ -57,14 +59,29 @@ ttaxa = AbstractFloraCorpusReader(reader=trdr)
 
 with open('flg.fcfg', 'r', encoding='utf-8') as gf:
     gs = gf.read()
-# pr = FGFlora.fromstring(gs, fltokens=sentence.tokens)
 
-alltokens = [tk for taxon in ttaxa.taxa for sent in taxon.sentences for tk in sent.tokens]
-pr = FGFlora.fromstring(gs, fltokens=alltokens)
-parser = parse.FeatureBottomUpLeftCornerChartParser(pr, trace=2)
-for taxon in ttaxa.taxa:
-    for sent in taxon.sentences:
-        for i, phrase in enumerate(sent.phrases):
-            trees = parser.parse(phrase.tokens)
-            for tree in trees:
-                print(tree)
+ttrace = 4
+of = sys.stdout
+if __name__ == '__main__':
+    if False:
+        ttrace = 0
+        ttaxa = FloraCorpusReader(db=r'..\resources\efloras.db3',
+                                  query="Select * from Taxa where genus = 'Salacia' and species = 'erecta';", )
+        of = open('testphrases.txt', 'w', encoding='utf-8')
+    alltokens = [tk for taxon in ttaxa.taxa for sent in taxon.sentences for tk in sent.tokens]
+    pr = FGFlora.fromstring(gs, fltokens=alltokens)
+    parser = parse.FeatureEarleyChartParser(pr, trace=ttrace)
+    for taxon in ttaxa.taxa:
+        for sent in taxon.sentences:
+            for i, phrase in enumerate(sent.phrases):
+                trees = list(parser.parse(phrase.tokens))
+                if trees:
+                    print('Success: ' + phrase.text, file=of)
+                    print('No. of trees: %d' % len(trees), file=of)
+                    if ttrace:
+                        for treex in trees:
+                            treex.draw()
+                else:
+                    print('Fail:    ' + phrase.text, file=of)
+    of.close()
+
